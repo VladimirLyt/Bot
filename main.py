be3aa3a2896717bbb5fb6f51d243c1d3c5e5afdb
lab3 <dom56f@gmail.com>
2023-05-26 20:56:48 +0900

    bot

--- main.py
+++ main.py
@@ -5,35 +5,82 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,'Привет, я бот созданный для объяснения предметов в игре Лига Легенд')
+	bot.send_message(message.chat.id,'Привет, я бот созданный для объясния игры Лига Легенд')
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	item1 = types.KeyboardButton("Страдания Лиандри")
-	item2 = types.KeyboardButton("Буря Людена")
-	item3 = types.KeyboardButton("Наш сервер")
-	markup.add(item1, item2, item3)
-	bot.send_message(message.chat.id, 'Выберите какой предмет вас интересует', parse_mode='html', reply_markup=markup)
+	item4 = types.KeyboardButton('Персонаж')
+	item5 = types.KeyboardButton('Предметы')
+	item8 = types.KeyboardButton('Наш сервер')
+	markup.add(item4,item5, item8)
+	bot.send_message(message.chat.id, 'Выберите какая категория вас интересует', parse_mode='html', reply_markup=markup)
+
 @bot.message_handler(content_types=['text'])
 def message_reply(message):
 	# Буря Людена инф
 	with open('1.txt', 'r', encoding='utf-8') as f:
 		stuff= f.read()
 	photo = open('буря людена.webp', 'rb')
+	with open('lux.txt', 'r', encoding='utf-8') as f:
+		luxt = f.read()
+	lux = open('люкс.webp', 'rb')
 	# Страдания Лиандры инф
 	with open('2.txt', 'r', encoding='utf-8') as f:
 		stuff1 = f.read()
 	photo1 = open('Страдания Лиандри.webp', 'rb')
+	if message.text == 'Персонаж':
+		markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item6 = types.KeyboardButton('Люкс')
+		markup2.add(item6)
+		bot.send_message(message.chat.id, 'Персонажи', parse_mode='html', reply_markup=markup2)
+	elif message.text == 'Предметы':
+		markup3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item1 = types.KeyboardButton("Страдания Лиандри")
+		item2 = types.KeyboardButton("Буря Людена")
+		markup3.add(item1, item2)
+		bot.send_message(message.chat.id, 'Предметы', parse_mode='html', reply_markup=markup3)
+	elif message.text == 'Люкс':
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item4 = types.KeyboardButton('Персонаж')
+		item5 = types.KeyboardButton('Предметы')
+		markup.add(item4, item5)
+		bot.send_message(message.chat.id, 'Выберите какая категория вас интересует', parse_mode='html',
+						 reply_markup=markup)
+
+		bot.send_photo(message.chat.id, lux)
+		bot.send_message(message.chat.id, luxt)
+		bot.send_message(message.chat.id, parse_mode='html', reply_markup=markup)
 	#Сервер
-	if message.text=="Наш сервер":
+	elif message.text=="Наш сервер":
 		bot.send_message(message.chat.id,"https://discord.gg/jtESKcQt/")
+
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item4 = types.KeyboardButton('Персонаж')
+		item5 = types.KeyboardButton('Предметы')
+		markup.add(item4, item5)
+		bot.send_message(message.chat.id, 'Выберите какая категория вас интересует', parse_mode='html',
+						 reply_markup=markup)
 	#Буря Людена
 	elif message.text == "Буря Людена":
 		bot.send_photo(message.chat.id, photo)
 		bot.send_message(message.chat.id, stuff)
+
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item4 = types.KeyboardButton('Персонаж')
+		item5 = types.KeyboardButton('Предметы')
+		markup.add(item4, item5)
+		bot.send_message(message.chat.id, 'Выберите какая категория вас интересует', parse_mode='html',
+						 reply_markup=markup)
 	#Страдания Лиандры
 	elif message.text == "Страдания Лиандри":
 		bot.send_photo(message.chat.id, photo1)
 		bot.send_message(message.chat.id, stuff1)
+
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		item4 = types.KeyboardButton('Персонаж')
+		item5 = types.KeyboardButton('Предметы')
+		markup.add(item4, item5)
+		bot.send_message(message.chat.id, 'Выберите какая категория вас интересует', parse_mode='html',
+						 reply_markup=markup)
 	#Ошибка
 	else:
 		bot.send_message(message.chat.id, 'Выберите из предложенного списка')
-bot.polling(none_stop=True, interval=0)
+bot.infinity_polling(none_stop=True, interval=0)
