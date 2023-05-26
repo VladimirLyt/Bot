19c052985b9b33d96d5d87234e01d0f85298fb43
lab3 <dom56f@gmail.com>
2023-05-26 10:35:27 +0900

    bot

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
+token = '6019618022:AAHpQmczWOJXOKaVpyO5ec6jFRh9S_lHOAI'
+bot = telebot.TeleBot(token)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,'Привет, я бот созданный для объяснения предметов в игре Лига Легенд')
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1 = types.KeyboardButton("Страдания Лиандри")
+	item2 = types.KeyboardButton("Буря Людена")
+	item3 = types.KeyboardButton("Наш сервер")
+	markup.add(item1, item2, item3)
+	bot.send_message(message.chat.id, 'Выберите какой предмет вас интересует', parse_mode='html', reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+	# Буря Людена инф
+	with open('1.txt', 'r', encoding='utf-8') as f:
+		stuff= f.read()
+	photo = open('буря людена.webp', 'rb')
+	# Страдания Лиандры инф
+	with open('2.txt', 'r', encoding='utf-8') as f:
+		stuff1 = f.read()
+	photo1 = open('Страдания Лиандри.webp', 'rb')
+	#Сервер
+	if message.text=="Наш сервер":
+		bot.send_message(message.chat.id,"https://discord.gg/jtESKcQt/")
+	#Буря Людена
+	elif message.text == "Буря Людена":
+		bot.send_photo(message.chat.id, photo)
+		bot.send_message(message.chat.id, stuff)
+	#Страдания Лиандры
+	elif message.text == "Страдания Лиандри":
+		bot.send_photo(message.chat.id, photo1)
+		bot.send_message(message.chat.id, stuff1)
+	#Ошибка
+	else:
+		bot.send_message(message.chat.id, 'Выберите из предложенного списка')
+bot.polling(none_stop=True, interval=0)
